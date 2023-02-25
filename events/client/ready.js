149c93341939426e6859af75d4b9abd269919a93
TrustCyberID <94990282+TrustCyberID@users.noreply.github.com>
2023-02-26 05:06:09 +0700

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,13 +4,12 @@ module.exports = async (client) => {
     console.log(`[INFO] - ${client.user.username} (${client.user.id}) is Ready!`);
 
     const activity = {
-        name: `${client.prefix}play <songs>`,
+        name: `/play <songs>`,
         type: 5,
     };
 
     client.user.setPresence({ 
         activities: [activity], 
-        status: 'dnd', 
-        afk: true,
+        status: 'online'
     });
 };
