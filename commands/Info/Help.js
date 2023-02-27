664668cd7249ecc039019a947a3742f7e1c75f96
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-02-27 15:05:37 +0700

    Idk, I just make it fresher

--- commands/Info/Help.js
+++ commands/Info/Help.js
@@ -4,14 +4,13 @@ const { readdirSync } = require("fs");
 module.exports = {
     name: ["help"],
     description: "Display all commands bot has.",
-    category: "Misc",
+    category: "Info",
     run: async (client, interaction) => {
         const embed = new EmbedBuilder()
             .setColor(client.color)
-            .setAuthor({ name: `${interaction.guild.members.me.displayName} Help Command!`, iconURL: interaction.guild.iconURL({ dynamic: true })})
-            .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
+            .setAuthor({ name: `${interaction.guild.members.me.displayName} Help`, iconURL: interaction.guild.iconURL({ dynamic: true })})
             .setDescription(`The bot prefix is: **/**`)
-            .setFooter({ text: `© ${interaction.guild.members.me.displayName} | Total Commands: ${client.commands.size}`, iconURL: client.user.displayAvatarURL({ dynamic: true })})
+            .setFooter({ text: `${client.commands.size} Commands` })
 
         const categories = readdirSync("./commands/");
         categories.forEach(category => {
@@ -19,7 +18,7 @@ module.exports = {
             const capitalise = category.slice(0, 1).toUpperCase() + category.slice(1);
 
             try {
-                embed.addFields({ name: `❯ ${capitalise} [${dir.size}]:`, value: `${dir.map(c => `\`${c.name.at(-1)}\``).join(", ")}`, inline: false })
+                embed.addFields({ name: `[${dir.size}] ${capitalise}`, value: `${dir.map(c => `\`${c.name.at(-1)}\``).join(", ")}`, inline: false })
             } catch(e) {
                 console.log(e)
             }
